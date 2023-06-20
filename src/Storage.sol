d85bde7ec20f9a149c42a4e490cc067ce7617259
Steven Rico <dev.stevenrico@proton.me>
2023-06-20 23:57:27 +0100

    feat(Storage): get a value from storage

--- src/Storage.sol
+++ src/Storage.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.19;
 
 contract Storage {
-  uint256 private _a;
+  uint256 private _a = 5;
+  uint256 private _b = 10;
+  uint256 private _c = 100;
 
   function storageLocation() external pure returns (uint256) {
     uint256 slotLocation;
@@ -12,5 +14,15 @@ contract Storage {
     }
 
     return slotLocation;
+  }
+
+  function getAtSlot(uint256 slot) external view returns (uint256) {
+    uint256 value;
+
+    assembly {
+      value := sload(slot)
+    }
+
+    return value;
   }
 }
--- test/Storage.t.sol
+++ test/Storage.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import { Test } from "forge-std/Test.sol";
+import { Test, console } from "forge-std/Test.sol";
 import { Storage } from "../src/Storage.sol";
 
 contract StorageTest is Test {
@@ -13,5 +13,12 @@ contract StorageTest is Test {
 
   function testStorageLocation() external {
     assertEq(_storage.storageLocation(), 0);
+  }
+
+  function testGetAtSlot() external {
+    assertEq(_storage.getAtSlot(0), 5);
+    assertEq(_storage.getAtSlot(1), 10);
+    assertEq(_storage.getAtSlot(2), 100);
+    assertEq(_storage.getAtSlot(10), 0);
   }
 }
