e306328c2f04f2e604f6c60f9ab82648b47be87a
Steven Rico <dev.stevenrico@proton.me>
2023-06-21 19:55:20 +0100

    refactor(Storage): remove unused console

--- test/Storage.t.sol
+++ test/Storage.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import { Test, console } from "forge-std/Test.sol";
+import { Test } from "forge-std/Test.sol";
 import { Storage } from "../src/Storage.sol";
 
 contract StorageTest is Test {
